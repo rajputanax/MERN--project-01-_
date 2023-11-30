8adb0e8a3d8f99c8573053ddbc52b0d8c3370da9
IbrahimAnas <anas.jameel.dev@pk.see.biz>
2023-11-30 20:28:32 +0500

    login():wip

--- routes/controller/userControllers.js
+++ routes/controller/userControllers.js
@@ -10,13 +10,7 @@ const getMe = asyncHandler(
     }
 )
 
-// .................................
 
-const loginUser = asyncHandler(
-    async (req, res) => {
-        await res.status(200).json({ msg: "user logged in " })
-    }
-)
 // .................................
 
 const registerUser = asyncHandler(
@@ -24,7 +18,7 @@ const registerUser = asyncHandler(
         // ...............................................for valid req
 
         const { name, email, password } = req.body
-        console.log(req.body)
+        // console.log(req.body)
         if (!name || !email || !password) {
             res.status(400)
             throw new Error('plz fill all fields ')
@@ -33,39 +27,58 @@ const registerUser = asyncHandler(
 
         const userExsist =await userData.findOne({ email });
         if (userExsist) {
-            console.log(userExsist)
+            // console.log(userExsist)
             res.status(404)
             throw new Error('this user already exsist')
         }
         // ..............................................convert pwd into hashed form using bcrypt
         // ..............................................gen salt builtin bcrypt method
-
         const salt = await bcrypt.genSalt(10)
         const hashedPwd = await bcrypt.hash(password, salt)
-
         // ...............................................create user
         const user = await userData.create({
             name,
             email,
             password: hashedPwd
-        }
-        
-        )
-
-        console.log(user)
-        // checking data is valid
+        })
+        //  console.log(user)
+        // checking data is valid then show it or 
         if (user) {
             res.status(201).json({
                 _id: user.id,
                 name: user.name,
                 email: user.email
             })
-        } else {
+        } 
+        //  if something goes wrong than show error
+        else {
             res.status(400)
             throw new Error('invalid user data')
         }
-
     }
 )
+
+// .................................
+
+const loginUser = asyncHandler(
+    async (req, res) => {
+const {email , password} = req.body
+console.log(req.body)
+const user = await userData.findOne({email})
+if (user && (await bcrypt.compare(password, user.password)) ){
+    res.json({
+        _id:user.id,
+        name:user.name,
+        email:user.eamil
+    })
+}
+else{
+    res.status(400)
+  throw new Error('invalid credentials dude')
+}
+    }
+) 
+
+
 // .................................
 module.exports = { getMe, loginUser, registerUser };
