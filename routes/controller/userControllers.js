f85febb98314c111f4ea758e25006b8aa1379778
IbrahimAnas <anas.jameel.dev@pk.see.biz>
2023-12-06 19:31:08 +0500

    jhgf

--- routes/controller/userControllers.js
+++ routes/controller/userControllers.js
@@ -2,17 +2,24 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
 const asyncHandler = require('express-async-handler')
 const userData = require('../../model/userModel')
+require('dotenv').config(process.env.JWT_SECRET);
+const JWT_SECRET = process.env.JWT_SECRET;
+// Now you can use JWT_SECRET in your code
+ console.log(JWT_SECRET);
 // .................................
-
+//[private route]
 const getMe = asyncHandler(
     async (req, res) => {
         await res.status(200).json({ msg: "users data displayed me" })
     }
 )
-
-
-// .................................
-
+// get token.....................
+const generateToken = (id) => {
+    return jwt.sign({ id }, JWT_SECRET, {
+        expiresIn: '30d',
+    });
+};
+// .................................//[public routes 👇]..............................................................
 const registerUser = asyncHandler(
     async (req, res) => {
         // ...............................................for valid req
@@ -25,14 +32,14 @@ const registerUser = asyncHandler(
         }
         //.............................................. user exsistance checker
 
-        const userExsist =await userData.findOne({ email });
+        const userExsist = await userData.findOne({ email });
         if (userExsist) {
             // console.log(userExsist)
             res.status(404)
             throw new Error('this user already exsist')
         }
-        // ..............................................convert pwd into hashed form using bcrypt
-        // ..............................................gen salt builtin bcrypt method
+        // .........convert pwd into hashed form using bcrypt gen salt builtin bcrypt method
+    
         const salt = await bcrypt.genSalt(10)
         const hashedPwd = await bcrypt.hash(password, salt)
         // ...............................................create user
@@ -47,9 +54,10 @@ const registerUser = asyncHandler(
             res.status(201).json({
                 _id: user.id,
                 name: user.name,
-                email: user.email
+                email: user.email,
+                tokken: generateToken(user._id)
             })
-        } 
+        }
         //  if something goes wrong than show error
         else {
             res.status(400)
@@ -57,27 +65,27 @@ const registerUser = asyncHandler(
         }
     }
 )
-
 // .................................
 
 const loginUser = asyncHandler(
     async (req, res) => {
-const {email , password} = req.body
-console.log(req.body)
-const user = await userData.findOne({email})
-if (user && (await bcrypt.compare(password, user.password)) ){
-    res.json({
-        _id:user.id,
-        name:user.name,
-        email:user.eamil
-    })
-}
-else{
-    res.status(400)
-  throw new Error('invalid credentials dude')
-}
+        const { email, password } = req.body
+        console.log(req.body)
+        const user = await userData.findOne({ email })
+        if (user && (await bcrypt.compare(password, user.password))) {
+            res.json({
+                _id: user.id,
+                name: user.name,
+                email: user.eamil,
+                tokken: generateToken(user._id)
+            })
+        }
+        else {
+            res.status(400)
+            throw new Error('invalid credentials dude')
+        }
     }
-) 
+)
 
 
 // .................................
